40bef87d3a5c6bf0f4f0d323420b513eb90b1686
Gouson <gouson@qq.com>
2021-06-01 16:17:10 +0800

    初始化项目

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
-const path=require('path')
-module.exports={
-    mode:'production',
-    entry:{
-        ice:'./lib/index.tsx'
-    },//程序的入口
-    output:{
-        path:path.resolve(__dirname,'dist/lib'),
-        library:'ICEUI',
-        libraryTarget:'umd'
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    mode: 'production',
+    entry: {
+        ice: './lib/index.tsx'
+    }, //程序的入口
+    output: {
+        path: path.resolve(__dirname, 'dist/lib'),
+        library: 'ICEUI',
+        libraryTarget: 'umd'
 
     },
-    module:{//如何解析
-        rules:[
-            {
-                test:/\.tsx?$/,
-                loader:'awesome-typescript-loader'
-            }
-        ]
-    }
+    module: { //如何解析
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'awesome-typescript-loader'
+        }]
+    },
+    plugins:[
+        new htmlWebpackPlugin({
+            title:'ICE',
+            template:'index.html'
+        })
+    ]
 }
