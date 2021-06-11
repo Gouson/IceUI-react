03667465e59319eab6e1af41f49c9a4035e6a1cb
Gouson <gouson@qq.com>
2021-06-11 17:22:59 +0800

    component

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,5 @@
-import  React from 'react'
-import  * as ReactDOM from 'react-dom'
-import Button from './button'
+import ReactDOM from 'react-dom'
+import React from 'react'
+import Icon from './icon'
 
-ReactDOM.render(<Button></Button>,document.body)
+ReactDOM.render(<div><Icon name="hello"></Icon></div>, document.body)
