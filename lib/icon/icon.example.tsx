8dc184f16e791f395a81b02960a4ec5658dbf4dc
Gouson <gouson@qq.com>
2021-06-17 18:17:23 +0800

    图标更新

--- lib/icon/icon.example.tsx
+++ lib/icon/icon.example.tsx
@@ -4,8 +4,12 @@ import Icon from './icon';
 const IconExample: React.FunctionComponent = () => {
     return (
         <div>
+             <Icon name="microsoft" />
+            <Icon name="google" />
             <Icon name="alipay" />
             <Icon name="wechat" />
+            <Icon name="qq" />
+            <Icon name="baidu" />
         </div>
     );
 };
