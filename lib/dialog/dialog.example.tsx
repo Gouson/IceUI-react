071d2f593e56114afbd446584c0ac203cd7234cc
Gouson <gouson@qq.com>
2021-07-06 18:27:31 +0800

    dialog

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,15 +1,41 @@
 import React, { useState } from 'react';
-import Dialog from './dialog'
+import Dialog, { alert } from './dialog'
 const DialogExample: React.FunctionComponent = () => {
     const [x, setX] = useState(false);
+    const [y, setY] = useState(false);
     return (
         <div>
-            <button onClick={() => { setX(!x) }}>切换</button>
-            <Dialog visible={x} >
-                <div>hi</div>
-            </Dialog>
+            <div>
+                <h1>1</h1>
+                <button onClick={() => { setX(!x) }}>切换</button>
+                <Dialog visible={x} buttons={[
+                    <button onClick={() => { setX(false) }}>确定</button>,
+                    <button onClick={() => { setX(false) }}>取消</button>
+                ]}
+                    onClose={() => { setX(false) }}
+                    closeOnClickMask={true}
+                >
+                    <div>hi</div>
+                </Dialog>
+            </div>
+            <div>
+                <h1>2</h1>
+                <button onClick={() => { setY(!y) }}>切换</button>
+                <Dialog visible={y} buttons={[
+                    <button onClick={() => { setY(false) }}>确定</button>,
+                    <button onClick={() => { setY(false) }}>取消</button>
+                ]}
+                    onClose={() => { setY(false) }}
+                >
+                    <div>hi</div>
+                </Dialog>
+            </div>
+            <div>
+                <h1>3</h1>
+                <button onClick={() => alert('1')}>点击</button>
+            </div>
         </div>
     );
 };
 
-export default DialogExample; 
+export default DialogExample;
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,33 +1,63 @@
 
 import { scopedClassMaker } from "../classes";
-import React, { Fragment } from "react";
+import React, { Fragment, ReactElement } from "react";
 import Icon from "../icon/icon";
 import './dialog.scss'
+import ReactDOM from "react-dom";
 interface Props {
     visible: boolean;
+    buttons?: Array<ReactElement>;
+    onClose: React.MouseEventHandler;
+    closeOnClickMask?: boolean
 }
 
 const scopedClass = scopedClassMaker('iceui-dialog')
 const sc = scopedClass
 const Dialog: React.FunctionComponent<Props> = (props) => {
-    return (
+    const onClickClose: React.MouseEventHandler = (e) => {
+        props.onClose(e)
+    }
+    const onClickMusk: React.MouseEventHandler = (e) => {
+        props.closeOnClickMask && props.onClose(e)
+    }
+    const x = (
         props.visible ?
             <Fragment>
-                <div className={sc('musk')}></div>
+                <div className={sc('musk')} onClick={onClickMusk}></div>
                 <div className={sc()}>
-                    <div className={sc("close")}>
+                    <div className={sc("close")} onClick={onClickClose}>
                         <Icon name="close"></Icon>
                     </div>
                     <header className={sc('header')}>提示</header>
                     <main className={sc('main')}>{props.children}</main>
                     <footer className={sc('footer')}>
-                        <button>1</button>
-                        <button>2</button>
+                        {props.buttons && props.buttons.map((button, index) => React.cloneElement(button, { key: index }))}
                     </footer>
                 </div>
             </Fragment>
             :
             null
     )
+    return ReactDOM.createPortal(
+        x,
+        document.body
+    )
 }
+
+Dialog.defaultProps = {
+    closeOnClickMask: false
+}
+
+const alert = (content: string,) => {
+    const component = <Dialog visible={true} onClose={() => {
+        ReactDOM.render(React.cloneElement(component, { visible: false }), div)
+        ReactDOM.unmountComponentAtNode(div)
+        div.remove()
+    }}>{content}</Dialog>
+    const div = document.createElement('div')
+    document.body.append(div)
+    ReactDOM.render(component, div)
+}
+
+export { alert }
 export default Dialog
